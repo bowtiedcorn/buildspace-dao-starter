eff40f5c83748441703ff7965b29127fbd8b762b
bowtiedcorn <bowtiedcorn@gmail.com>
2021-12-30 15:06:58 -0500

    small updates to app

--- src/App.jsx
+++ src/App.jsx
@@ -101,8 +101,6 @@ const App = () => {
       return;
     }
 
-    // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
-    // with tokenId 0.
     bundleDropModule
       .getAllClaimerAddresses("0")
       .then((addresess) => {
